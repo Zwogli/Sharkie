1035bad35cd61b345e446b4d8ee58962c82e527c
Zwogli <Mathiaskohler@web.de>
2023-06-22 09:03:18 +0200

    edit position_x

--- assets/models/hud/fullscreen.class.js
+++ assets/models/hud/fullscreen.class.js
@@ -5,8 +5,8 @@ IMAGE = './assets/img/icon/fullscreen.png';
 constructor(){
   super();
   this.loadImg(this.IMAGE);
-  this.x = 400;
-  this.y = 10;
+  this.x = 660;
+  this.y = 25;
   this.width = 48;
   this.height = 48;
 }
